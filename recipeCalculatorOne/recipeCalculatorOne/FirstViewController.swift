fdd2a7e6e7d1d01d40a257bb740400881d4dfdff
Rinni Swift <rinradaswift@gmail.com>
2018-07-25 13:34:26 -0700

    tabbed app starter project

---
+++ recipeCalculatorOne/recipeCalculatorOne/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  recipeCalculatorOne
+//
+//  Created by Rinni Swift on 7/25/18.
+//  Copyright © 2018 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ recipeCalculatorOne/recipeCalculatorOne/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  recipeCalculatorOne
+//
+//  Created by Rinni Swift on 7/25/18.
+//  Copyright © 2018 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
